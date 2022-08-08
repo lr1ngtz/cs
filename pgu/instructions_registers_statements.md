72afdbebc91ba2b2fc6d5e5dfafbf73810590aa9
@Pavel.Klachkou <pavel.klachkou@itechart-group.com>
2022-08-08 19:09:17 +0200

    Initial commit

---
+++ pgu/instructions_registers_statements.md
@@ -0,0 +1,9 @@
+## Instructions
+
+**je** - Jump if the values were equal
+**jg** - Jump if the second value was greater than the first value 12
+**jge** - Jump if the second value was greater than or equal to the first value
+**jl** - Jump if the second value was less than the first value
+**jle** - Jump if the second value was less than or equal to the first value
+**jmp** - Jump no matter what. This does not need to be preceeded by a comparison.
+
---
+++ pgu/maximum.s
@@ -0,0 +1,42 @@
+# VARIABLES: The registers have the following uses:
+#
+# %edi - Holds the index of the data item being examined
+# %ebx - Largest data item found
+# %eax - Current data item
+#
+# The following memory locations are used:
+#
+# data_items - contains the item data. A 0 is used
+# to terminate the data
+#
+ .section .data
+data_items: #These are the data items
+ .long 3,67,34,222,45,75,54,34,44,33,22,11,77
+
+ .section .text
+
+ .globl _start
+_start:
+  movl $0, %edi # move 0 into the index register
+  movl data_items(,%edi,4), %eax # (INDEXED ADDRESSING MODE) load the first byte of data 
+  movl %eax, %ebx # since this is the first item, %eax is the biggest
+  
+  start_loop: # start loop
+    # cmpl - compare, the result of cmpl stores in %eflags (status register)
+    cmpl $77, %eax # check to see if we’ve hit the end
+    # je - jump if equal, jump to the end_loop
+    je loop_exit
+
+    incl %edi # incl increments the value of %edi by one
+    movl data_items(,%edi,4), %eax
+    cmpl %ebx, %eax # compare values
+    jle start_loop # jump to loop beginning if the new one isn’t bigger
+                   
+    movl %eax, %ebx # move the value as the largestChapter
+    jmp start_loop # jump to loop beginning
+  loop_exit:
+    # %ebx is the status code for the exit system call
+    # and it already has the maximum number
+    movl $1, %eax #1 is the exit() syscall
+    int $0x80
+
