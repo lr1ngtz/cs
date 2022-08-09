eedb31dca17cbe1d78d87e39257b8fb4587627b3
@Pavel.Klachkou <pavel.klachkou@itechart-group.com>
2022-08-09 16:41:20 +0200

    2.2.1 Arithmetic operations, 2.2.2 Variables, initialization, assignments

---
+++ stolyarov/prog_intro_v1/assignments.pas
@@ -0,0 +1,12 @@
+program assignments;
+var
+	x: integer = 25;
+begin
+	writeln(x);
+	x := 12;
+	writeln(x);
+	x := 100;
+	writeln(x);
+	x := x + 20;
+	writeln(x)
+end.
---
+++ stolyarov/prog_intro_v1/div_mod.pas
@@ -0,0 +1,5 @@
+program div_mod;
+begin
+	writeln(27 div 4);  {* return 6*}
+	writeln(27 mod 4);  {* return 3*}
+end.
