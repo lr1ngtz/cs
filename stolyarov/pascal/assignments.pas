2ea8505bfe72af40e30c7b07e36dfb8eb6a1bbe9
@Pavel.Klachkou <pavel.klachkou@itechart-group.com>
2023-03-03 17:50:12 +0100

    Assignments, operators, read from stdin

---
+++ stolyarov/pascal/assignments.pas
@@ -0,0 +1,10 @@
+program assignments;
+var
+	x: integer = 5;
+begin
+	writeln(x);
+	x := 10;
+	writeln(x);
+	x := 20;
+	writeln(x);
+end.
---
+++ stolyarov/pascal/operators.pas
@@ -0,0 +1,8 @@
+program operators;
+begin
+	writeln(2 + 2);
+	writeln(2 * 4);
+	writeln(5 - 4);
+	writeln(14 / 7);
+	writeln(3 <> 4 - 22);
+end.
